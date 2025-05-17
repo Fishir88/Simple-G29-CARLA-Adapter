cb8de1561d28030fbb16d25cd5394afadb3305fd
Halir Szabolcs <halirszabi@gmail.com>
2025-05-17 18:17:28 +0200

    Gear switching implemented

--- g29_adapter/adapter.py
+++ g29_adapter/adapter.py
@@ -1,50 +1,62 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Bool, String
 
 class JoyProcessor(Node):
     def __init__(self):
         super().__init__('joy_processor')
         self.joy_sub = self.create_subscription(Joy, '/joy', self.joy_callback, 10)
-        
-        # Publishers for throttle and steering and braking
         self.throttle_pub = self.create_publisher(Float64, '/throttle_command', 10)
         self.steering_pub = self.create_publisher(Float64, '/steering_command', 10)
         self.brake_pub = self.create_publisher(Float64, '/brake_command', 10)
-        
+        self.handbrake_pub = self.create_publisher(Bool, '/handbrake_command', 10)
+        self.gear_pub = self.create_publisher(String, '/gear_command', 10)
+        self.get_logger().info(f"Adapter initialized, waiting for Joy messages...")
         self.latest_joy = None
+        self.first_joy_received = False
 
     def joy_callback(self, msg: Joy):
+        if not self.first_joy_received:
+            self.get_logger().info("Joy message received, adapter started!")
+            self.first_joy_received = True
         self.latest_joy = msg
-        self.get_logger().info(f"Received Joy message: axes={msg.axes}, buttons={msg.buttons}")
         
         # Process the Joy message and publish throttle and steering
         self.publish_throttle_and_steering(msg)
 
     def publish_throttle_and_steering(self, joy_msg: Joy):
         # Assuming axes[1] is throttle and axes[0] is steering
-        throttle_value = -(max(0.0, min(1.0, joy_msg.axes[4])))  # Clamp to [0.0, 1.0]
+        throttle_value = 1-(max(0.0, min(1.0, joy_msg.axes[4])))  # Clamp to [0.0, 1.0]
         steering_value = -(max(-1.0, min(1.0, joy_msg.axes[0])))  # Clamp to [-1.0, 1.0]
-        brake_value = -(max(0.0, min(1.0, joy_msg.buttons[5])))  # Clamp to [0.0, 1.0]
+        brake_value = 1-(max(0.0, min(1.0, joy_msg.axes[5])))  # Clamp to [0.0, 1.0]
 
         # Publish throttle
         throttle_msg = Float64()
         throttle_msg.data = throttle_value
         self.throttle_pub.publish(throttle_msg)
-        self.get_logger().info(f"Published throttle: {throttle_msg}")
 
         # Publish steering
         steering_msg = Float64()
         steering_msg.data = steering_value
         self.steering_pub.publish(steering_msg)
-        self.get_logger().info(f"Published steering: {steering_msg}")
         
         # Publish braking
         brake_msg = Float64()
         brake_msg.data = brake_value
         self.brake_pub.publish(brake_msg)
-        self.get_logger().info(f"Published brake: {brake_msg}")
+        
+        # Publish handbrake
+        handbrake_msg = Bool()
+        handbrake_msg.data = joy_msg.buttons[7] == 1
+        self.handbrake_pub.publish(handbrake_msg)
+        
+        # Publish gear
+        if joy_msg.buttons[0] == 1 or joy_msg.buttons[1] == 1:
+            gear_msg = String()
+            gear_msg.data = "reverse" if joy_msg.buttons[1] else "forward"
+            self.gear_pub.publish(gear_msg)
+        
 
 def main(args=None):
     rclpy.init(args=args)
